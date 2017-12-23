b58a7caa6c2317e5b184093d4e12eb10d6260db9
nithika <nithika1011@gmail.com>
2017-12-23 11:28:25 +0500

    merged the twitter-aadhar folder with chatbot

--- twitter_aadhar/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Aadhaar from './Aadhaar';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Aadhaar />, document.getElementById('root'));
-registerServiceWorker();
